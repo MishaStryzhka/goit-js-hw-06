f8e7a04e38c3088c8b990a283f6f20896cd161eb
MishaStryzhka <misha.strizhcko@gmail.com>
2023-02-01 10:55:54 +0100

    Update task-07

---
+++ js/task-07.js
@@ -0,0 +1,7 @@
+const fontSizeControl = document.getElementById("font-size-control");
+const text = document.getElementById("text");
+
+fontSizeControl.addEventListener("input", (event) => {
+    text.style.fontSize = `${event.currentTarget.value}px`;
+    }
+)
