a31ecb077a5112caf8c0c5844093745d21173dad
MishaStryzhka <misha.strizhcko@gmail.com>
2023-02-01 11:47:33 +0100

    Update task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', onFormSubmit)
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    console.log(event.currentTarget.elements[0].name)
+    if (event.currentTarget.elements[0].value === "" || event.currentTarget.elements[1].value === "") {
+        alert("Заповніть всі поля");
+    };
+
+    const mail = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.email.value;
+
+    console.log({mail, password});
+
+    form.reset()
+}
