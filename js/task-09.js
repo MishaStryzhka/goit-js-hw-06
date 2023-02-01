ee9a8a488c559fe734bc35fa99a9b49799109924
MishaStryzhka <misha.strizhcko@gmail.com>
2023-02-01 21:39:41 +0100

    Update task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const btnChangeColor = document.querySelector(".change-color");
+const textColor = document.querySelector(".color")
+
+btnChangeColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+
+  textColor.textContent = randomColor
+  document.body.style.backgroundColor = randomColor;
+  // console.log(randomColor)
+})
